b465f210bf316a8ce3a07d5ef5f84d53b5bf74bd
Yun Jung Hyeon <yjh54240@naver.com>
2018-04-08 22:54:36 +0900

    Single 클래스 설명 추가

--- RxJava/RxJava 연산자/Source/TransformingOp/src/FlatMapExample.java
+++ RxJava/RxJava 연산자/Source/TransformingOp/src/FlatMapExample.java
@@ -15,27 +15,27 @@ public class FlatMapExample {
 
         Observable<String> observable=Observable.fromArray(sports)
                 .flatMap(str-> Observable.just("운동: "+str));
-
         observable.subscribe(System.out::println);
 
-
-        //rxjava1 에서는 RxJavaMath가 있었지만 rxjava2에서는 지원을 안해서 RxJava2Extensions 라이브러리를 등록해야한다.
-/*
-        Observable.fromArray(1,2,4,3).to(s->MathObservable.max(s))
-                .subscribe(max->System.out.println(max));
-
-        Flowable.fromArray(integers).to(MathFlowable::max)
-                .subscribe(max->System.out.println(max));
-*/
-
+        System.out.println("----------------------");
 
         //observable를 리턴해서 MathObservable 인자로 넘겨줌
-        Observable<String> sumObservable=Observable.just(0)
-                .flatMap(i ->Observable.range(1,9))
+        int result=0; //초기값
+        Observable<String> sumObservable=Observable.just(result)
+                .flatMap(num -> Observable.range(1,9)
                 .to(flatMapObservable-> MathObservable.sumInt(flatMapObservable)
-                .map(sum -> "총합은 "+sum));
-
+                .map(sum -> "총합은 "+(sum+result))));
         sumObservable.subscribe(sum->System.out.println(sum));
+
+        System.out.println("----------------------");
+        //구구단
+        int dan=3;
+        Observable<String> guguDan=Observable.just(dan)
+                .flatMap(num->Observable.range(1,9)
+                        .map(row-> num+"*"+row+"="+row*num));
+
+        guguDan.subscribe(System.out::println);
+
 
 
     }
---
+++ RxJava/RxJava 클래스/Single.md
@@ -0,0 +1,29 @@
+# Single 클래스
+RxJava 1.x 버전부터 존재하였고 
+Observable 특수한 형태로 오직 1개의 데이터만 발행하도록 제한한다. 
+주로 보통 **서버 APi 호줄**할 때 사용된다. (즉 클라이언트 요청하나에 대한 결과 하나만 받으면 된다.)
+
+## Single 흐름의 단계  
+Observable에서의 onNext(), onComplete() 함수가 
+onSuccess() 함수로 통합되었다. 
+
+즉 **onSuccess()**, **onError()** 로 흐름이 진행된다. 
+
+## 사용방법 
+
+<pre>
+Single< String >  single=Single.just("Hello");
+single.subscribe(System.out::println);
+
+결과 : Hello
+</pre>
+
+Obserable 하고 똑같이 사용하면되고 단지 주위할 점은 Single로 **여러개의 데이터가 발행**되면 오류가 뜨니간 이점은 조심해야된다.  
+
+
+## Single 클래스 활용
+
+1. Obserable에서 Single 객체로 변환 
+2. 
+
+## 참고자료
